d6f37101c8cafd1874631a6d7f495622774e5f03
SandervdSpree <sandervanderspree@hotmail.com>
2019-10-21 11:31:00 +0200

    Add files via upload
    Added SD-card logger for logging during off-time

--- WiFiClientWorking.ino
+++ WiFiClientWorking.ino
@@ -10,8 +10,10 @@
 #include <ESP8266WiFi.h>
 
 #ifndef STASSID
-#define STASSID "CIM_Arduino"
-#define STAPSK  "d26bedfab3"
+//#define STASSID "CIM_Arduino"
+//#define STAPSK  "d26bedfab3"
+#define STASSID "CIMSOLUTIONS Guest"
+#define STAPSK  "Airport02"
 #endif
 
 #define NTP_OFFSET   60 * 60      // In seconds
@@ -23,20 +25,32 @@ const char* password = STAPSK;
 
 const char* host = "172.16.0.45";
 
-String data;
-boolean send = false;
+//const char* getHost = "10.5.0.205"; // The data receiver host name (not URL)
+//const int httpGetPort = 80; // The data receiver host port
+String getReceiverURL = "/"; // The data receiver script
+
+//WiFiServer server(80); 
+
+String data, latestTime;
+bool send = false;
 float temp,hum,lon,lat;
+String gpsTime;
+bool active = false;
+int research, id = 0;
 
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP, NTP_ADDRESS, NTP_OFFSET, NTP_INTERVAL);
 
 void setup()
 {
-  Serial.begin(115200);
-  Serial.println();
+  // initialize serial port:
+  Serial.begin(9600);
+  while(!Serial);
 
+  // connect to Wi-Fi
   Serial.printf("Connecting to %s ", ssid);
   WiFi.begin(ssid, password);
+  // start Time client
   timeClient.begin();
   while (WiFi.status() != WL_CONNECTED)
   {
@@ -44,14 +58,23 @@ void setup()
     Serial.print(".");
   }
   Serial.println(" connected");
+  Serial.println(WiFi.localIP());
+
+  Serial.printf("Web server started, open %s in a web browser\n", WiFi.localIP().toString().c_str());
 }
 
 
 void loop()
 {
-    // 22.6:55.6:4.603:52.506
+    // Test message for POST command on serial: 22.6:55.6:4.603:52.506
+    // Structure for activating weather station : "status:research:id"
+    // Status can be 0 or 1, off and on.
+  // check if there is a message on the serial
   while (Serial.available() > 0)
     {
+        // convert message to sensor values
+        gpsTime = Serial.readStringUntil('Z') + "Z";
+        Serial.readStringUntil(':');
         temp = Serial.readStringUntil(':').toFloat();
         hum = Serial.readStringUntil(':').toFloat();
         lon = Serial.readStringUntil(':').toFloat();
@@ -59,29 +82,39 @@ void loop()
         send = true;
     }
   if(send){
-      while(!timeClient.update()) {
-        timeClient.forceUpdate();
-      }
-      timeClient.setTimeOffset(7200);
-      DynamicJsonDocument doc(1024);
-      JsonObject humi  = doc.createNestedObject("humidity");
-      humi["value"] = hum;
-      doc["latitude"] = lat;
-      doc["longitude"] = lon;
-      JsonObject tempi = doc.createNestedObject("temperature");
-      tempi["value"] = temp;
-      doc["time"] = timeClient.getFormattedDate();
-      serializeJson(doc, Serial);
-      Serial.println();
+    postData();
+    send = false;
+  }
+}
 
+void postData(){
+    // start wifi client for sending
     WiFiClient client;
+    // get the current time
+    while(!timeClient.update()) {
+      timeClient.forceUpdate();
+    }
+    // compensate for offset on the world
+    timeClient.setTimeOffset(7200);
+    // create json structure for data
+    DynamicJsonDocument doc(1024);
+    JsonObject humi  = doc.createNestedObject("humidity");
+    humi["value"] = hum;
+    doc["latitude"] = lat;
+    doc["longitude"] = lon;
+    JsonObject tempi = doc.createNestedObject("temperature");
+    tempi["value"] = temp;
+    latestTime = timeClient.getFormattedDate();
+    //doc["time"] = latestTime;
+    doc["time"] = gpsTime;
+    serializeJson(doc, Serial);
+    Serial.println();
   
     Serial.printf("\n[Connecting to %s ... ", host);
     if (client.connect(host, 8080))
     {
       Serial.println("connected]");
-  
-      Serial.println("[Posting sensor values]");
+      delay(250);
       client.println("POST /research/3/voyager/1 HTTP/1.1");
       client.println("content-type: application/json");
       client.print("content-length: ");
@@ -94,12 +127,14 @@ void loop()
       client.println();
       //client.println(data);
       Serial.println("[Response:]");
+      delay(100);
       while (client.connected() || client.available())
       {
         if (client.available())
         {
           String line = client.readStringUntil('\n');
           Serial.println(line);
+          delay(100);
         }
       }
       client.stop();
@@ -110,6 +145,4 @@ void loop()
       Serial.println("connection failed!]");
       client.stop();
     }
-    send = false;
-  }
 }
